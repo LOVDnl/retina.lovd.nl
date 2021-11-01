858eec02e26131a33123cc2fde50bc8407d7d647
Ivo Fokkema <Ivo@IFokkema.nl>
2021-11-01 15:44:28 +0100

    Add framework that I copied from epoxytips.nl.
    This code is written by and licensed to myself; Ivo Fokkema
    <Ivo@IFokkema.nl> and I'm licensing this to the Leiden University
    Medical Center for use on retina.lovd.nl.

---
+++ inc-lib.php
@@ -0,0 +1,569 @@
+<?php
+// All of this code is copyrighted by Ivo Fokkema <Ivo@IFokkema.nl>,
+//  and developed and written for his private website epoxytips.nl.
+// This code has been licensed to the Leiden University Medical Center
+//  for use on retina.lovd.nl.
+
+function tips_displayError ($sTitle, $sError)
+{
+    // Display error.
+    print('
+        <div class="card border-danger mb-3">
+          <div class="card-header">' . $sTitle . '</div>
+          <div class="card-body text-danger">
+            <p class="card-text">' . $sError . '</p>
+          </div>
+        </div>');
+    return true;
+}
+
+
+
+
+
+function tips_formatPost ($aRequest)
+{
+    global $_DATA;
+    // Formats a post.
+
+    // Prepare body, replacing Markdown link format to HTML links.
+    $sBody = implode("\n                ",
+        array_map(
+            function ($sLine) {
+                if (preg_match('/\[([^\]]+)\]\(([^\)]+)\)/', $sLine, $aRegs)) {
+                    // Link found.
+                    // This code works only for the first link on this line.
+                    list($sPattern, $sTitle, $sURL) = $aRegs;
+                    $bInternal = (substr($sURL, 0, 4) != 'http'
+                        && substr($sURL, 0, 2) != 'l/');
+                    $sLine = str_replace(
+                        $sPattern,
+                        '<A href="' . $sURL . '"' . ($bInternal? '' : ' target="_blank"') . '>' .
+                            $sTitle . '</A>',
+                        $sLine
+                    );
+                }
+                return $sLine;
+            }, $aRequest['body']));
+    $sCategories = '';
+    $sTags = '';
+
+    foreach ($aRequest['category'] as $sCategory) {
+        $sCategories .= (!$sCategories? '' : ' > ') .
+            '<a href="c/' . tips_getLink($sCategory) . '" class="card-link">' . ucfirst($sCategory) . '</a>';
+    }
+    foreach ($aRequest['tags'] as $sTag) {
+        $sTags .= '<a href="t/' . tips_getLink($sTag) . '" class="card-link">#' . tips_getLink($sTag) . '</a>';
+    }
+
+    $sPost = '
+        <div class="card shadow-sm">
+            <div class="card-header">' .
+        ((time() - strtotime($aRequest['date'])) > (7 * 24 * 60 * 60)? '' : '
+                <span class="float-right badge badge-success">Recently updated!</span>') . '
+                <small class="text-muted">' . $sCategories . '</small>
+                <h4 class="card-title">' . $aRequest['title'] . '</h4>
+            </div>
+            <div class="card-body">
+                ' . $sBody . '
+            </div>
+            <div class="card-footer text-muted">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>' .
+        (!$sTags? '' : '
+                        ' . $sTags) . '
+
+                    </div>
+                    <small class="text-muted">last updated ' . $aRequest['date'] . '</small>
+                </div>' .
+        (true || empty($aRequest['code'])? '' : '
+                <span class="badge badge-primary">https://epoxytips.nl/' . $aRequest['code'] . '</span>') . '
+            </div>
+        </div>';
+
+    if (!empty($aRequest['related'])) {
+        $sRelatedLinks = '';
+        foreach ($aRequest['related'] as $sRelated) {
+            // Fetch last updated date from target page.
+            $sDate = '1970-01-01';
+            foreach ($_DATA['tips'] as $aTip) {
+                if ($aTip['title'] == $sRelated) {
+                    $sDate = $aTip['date'];
+                    break;
+                }
+            }
+            $sRelatedLinks .= '
+                <li class="list-group-item">' .
+                ((time() - strtotime($sDate)) > (7 * 24 * 60 * 60)? '' : '
+                    <span class="float-right badge badge-success">Recently updated!</span>') . '
+                    <a href="' . tips_getLink($sRelated) . '">' . $sRelated . '</a></li>';
+        }
+        $sPost .= '
+        <br><br>
+        <div class="card border-primary">
+            <div class="card-header">
+                Related links
+            </div>
+            <ul class="list-group list-group-flush">' .
+            $sRelatedLinks . '
+            </ul>
+        </div>';
+    }
+
+    return $sPost;
+}
+
+
+
+
+
+function tips_getCodes ()
+{
+    // Get all codes from the tips and return them.
+    global $_DATA;
+    static $aCodes = array();
+
+    if (empty($aCodes)) {
+        foreach ($_DATA['tips'] as $aTip) {
+            if (!empty($aTip['code'])) {
+                $aCodes[$aTip['code']] = tips_getLink($aTip['title']);
+            }
+        }
+    }
+
+    return $aCodes;
+}
+
+
+
+
+
+function tips_getTitles ()
+{
+    // Get all titles from the tips and return them with their tips attached.
+    global $_DATA;
+    static $aTitles = array();
+
+    if (empty($aTitles)) {
+        foreach ($_DATA['tips'] as $aTip) {
+            // Deliberately overwrite title if we already have it.
+            // I should not have created two of the same pages.
+            // I might as well just take the entire database here.
+            $aTitles[tips_getLink($aTip['title'])] = $aTip;
+        }
+    }
+
+    return $aTitles;
+}
+
+
+
+
+
+function tips_getInstallURL ($bFull = true)
+{
+    // Returns URL that can be used in URLs or redirects.
+    // ROOT_PATH can be relative or absolute.
+    return (!$bFull? '' : PROTOCOL . $_SERVER['HTTP_HOST']) .
+        str_replace(array(
+            '/./',
+            '//'
+        ), array(
+            '/',
+            '/'
+        ), substr(ROOT_PATH, 0, 1) == '/'? ROOT_PATH : dirname($_SERVER['SCRIPT_NAME']) . '/' . ROOT_PATH);
+}
+
+
+
+
+
+function tips_getLink ($sTitle)
+{
+    // Makes a link from a title.
+    static $aLinks = array();
+
+    if (!isset($aLinks[$sTitle])) {
+        $sLink = str_replace(array(
+            ' ',
+            ':',
+        ), array(
+            '-',
+            '-',
+        ), preg_replace(
+            '/[^a-z0-9 :_-]/',
+            '',
+            // Get rid of accents on characters.
+            iconv('UTF-8', 'ASCII//TRANSLIT', strtolower($sTitle))));
+        $aLinks[$sTitle] = $sLink;
+    }
+
+    return $aLinks[$sTitle];
+}
+
+
+
+
+
+function tips_setOGData ($aData)
+{
+    // Sets the Open Graph data for this page.
+
+    $sContents = ob_get_contents();
+    foreach (array('title', 'description') as $sProperty) {
+        if (isset($aData[$sProperty])) {
+            // Currently allows only empty contents to be filled in.
+            $sContents = str_replace(
+                '"og:' . $sProperty . '" content=""',
+                '"og:' . $sProperty . '" content="' . $aData[$sProperty] . '"',
+                $sContents);
+        }
+    }
+
+    // Replace the buffer.
+    ob_clean();
+    echo $sContents;
+    return true;
+}
+
+
+
+
+
+function tips_showCategories ($sURL)
+{
+    // Show categories or category, with links to posts.
+    global $_DATA;
+
+    // URL starts with 'c/' or is just 'c'.
+    $sCategory = strtolower(substr($sURL, 2));
+    $bShowAll = (!$sCategory);
+
+    if ($bShowAll) {
+        // No category requested. Fetch list to get order right.
+        $aCategories = array_combine(
+            $_DATA['categories'],
+            array_fill(0, count($_DATA['categories']), array()));
+    } else {
+        $aCategories = array();
+    }
+
+    foreach ($_DATA['tips'] as $aTip) {
+        // FIXME: Currently just allowing one-level categories.
+        if ($bShowAll || $sCategory == $aTip['category'][0]) {
+            $aCategories[$aTip['category'][0]][] = array($aTip['title'], $aTip['date']);
+        }
+    }
+
+    if (!$aCategories) {
+        // Wrong or non-existent category.
+        tips_displayError('Unknown category', 'This category is unknown. Go back to the home page or use the search form to find what you\'re looking for.');
+    }
+
+    // Fill in OG data to make the Facebook previews pretty.
+    tips_setOGData(array(
+        'title' => ($bShowAll? 'All categories' : 'Category: ' . $sCategory),
+    ));
+
+    $sPost = '';
+    foreach ($aCategories as $sCategory => $aTitles) {
+        $sCategoryLinks = '';
+        if (!$aTitles) {
+            // Empty category. Just skip, that's easier.
+            continue;
+        }
+        foreach ($aTitles as list($sTitle, $sDate)) {
+            $sCategoryLinks .= '
+            <li class="list-group-item">' .
+                ((time() - strtotime($sDate)) > (7 * 24 * 60 * 60)? '' : '
+                <span class="float-right badge badge-success">Recently updated!</span>') . '
+                <a href="' . tips_getLink($sTitle) . '">' . $sTitle . '</a>
+            </li>';
+        }
+        $sPost .= (!$sPost? '' : '
+        <BR><BR>') . '
+        <div class="card border-primary">
+            <div class="card-header">
+                ' . ucfirst($sCategory) . '
+            </div>
+            <ul class="list-group list-group-flush">' .
+                $sCategoryLinks . '
+            </ul>
+        </div>';
+    }
+
+    echo $sPost;
+}
+
+
+
+
+
+function tips_showLinks ($sURL)
+{
+    // Show links or forward to target page.
+    global $_DATA;
+
+    // URL starts with 'l/' or is just 'l'.
+    $sLink = strtolower(substr($sURL, 2));
+    $bShowAll = (!$sLink);
+
+    if ($bShowAll) {
+        // No specific link requested. Fetch list and display.
+        $aLinks = $_DATA['links'];
+    } else {
+        $aLinks = array();
+        foreach ($_DATA['links'] as $sKey => $sURL) {
+            if ($sKey == $sLink) {
+                // Full match. Take only this one.
+                $aLinks = array($sKey => $sURL);
+                break;
+            } elseif (substr($sKey, 0, strlen($sLink)) == $sLink) {
+                // Prefix match. Add it to list.
+                $aLinks[$sKey] = $sURL;
+            }
+        }
+    }
+
+    // FIXME: You could try and first match using similarity scores.
+    if (!$aLinks) {
+        // Wrong or non-existent link.
+        // Log this.
+        $sReferrer = (!isset($_SERVER['HTTP_REFERER'])? '' : $_SERVER['HTTP_REFERER']);
+        // @file_put_contents(
+        //     ROOT_PATH . 'logs/' . date('Y-m') . '.log',
+        //     date('Y-m-d\TH:i:s') . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['REQUEST_URI'] . "\t" . $sReferrer . "\t" . 'Error: Link not found.' . "\n",
+        //     FILE_APPEND|LOCK_EX
+        // );
+
+        tips_displayError('Unknown link', 'This link is unknown. If you have followed a link from our page, then this is an error on our side.');
+        return false;
+    }
+
+    // Fill in OG data to make the Facebook previews pretty.
+    tips_setOGData(array(
+        'title' => ($bShowAll? 'All links' : 'Link: ' . $sLink),
+    ));
+
+    if (count($aLinks) == 1) {
+        // Just one link, go for it.
+        header('Location: ' . current($aLinks), true, 302);
+        exit;
+    }
+
+    $sPost = '
+        <div class="card border-primary">
+            <div class="card-header">
+                Links matching the request
+            </div>
+            <ul class="list-group list-group-flush">';
+    foreach ($aLinks as $sKey => $sURL) {
+        $sPost .= '
+            <li class="list-group-item">
+                <a href="l/' . $sKey . '">' . $sKey . '</a>
+            </li>';
+    }
+    $sPost .= '
+            </ul>
+        </div>';
+
+    echo $sPost;
+}
+
+
+
+
+
+function tips_showPost ($aRequest)
+{
+    // Show a page.
+    global $_DATA;
+
+    if (!isset($aRequest['body'])) {
+        // We don't have a body, so try and find the page.
+        if (isset($aRequest['code'])) {
+            // Find the page by te code.
+            foreach ($_DATA['tips'] as $aTip) {
+                if (isset($aTip['code']) && $aTip['code'] == $aRequest['code']) {
+                    $aRequest = $aTip;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!isset($aRequest['body'])) {
+        tips_displayError('Page not found', 'I can not find this page. Go back to the home page or use the search form to find what you\'re looking for.');
+        return false;
+    }
+
+    // Fill in OG data to make the Facebook previews pretty.
+    tips_setOGData(array(
+        'title' => $aRequest['title'],
+        'description' => rtrim(strip_tags($aRequest['body'][0]), '.,') . '...',
+    ));
+
+    print tips_formatPost($aRequest);
+    return true;
+}
+
+
+
+
+
+function tips_showSearchResults ($sURL)
+{
+    // Show search results with links to posts.
+    global $_DATA;
+
+    // URL starts with 's/' or is just 's'.
+    $sSearchTerms = strtolower(substr($sURL, 2));
+    $aResults = array();
+
+    if (!$sSearchTerms) {
+        tips_displayError('No search term', 'You have not provided a search term. Fill in a search term in the field in the menu on the top of the page, and press &quot;Search&quot;.');
+        return false;
+    }
+
+    foreach ($_DATA['tips'] as $aTip) {
+        // Bonus points (x10) if the page's code matches,
+        //  extra points (x5) if the subject matches,
+        //  extra points (x2) if the page's tags match, otherwise
+        //  just one point each time the body matches.
+        $nScore = 0;
+        foreach (explode(' ', $sSearchTerms) as $sSearchTerm) {
+            $nScore += substr_count($aTip['code'], $sSearchTerm) * 10;
+            $nScore += substr_count(strtolower($aTip['title']), $sSearchTerm) * 5;
+            foreach ($aTip['tags'] as $sTag) {
+                $nScore += substr_count(strtolower($sTag), $sSearchTerm) * 2;
+            }
+            foreach ($aTip['body'] as $sLine) {
+                $nScore += substr_count(strtolower($sLine), $sSearchTerm);
+            }
+        }
+        if ($nScore > 0) {
+            $aTip['score'] = $nScore;
+            $aResults[] = $aTip;
+        }
+    }
+    usort($aResults, function ($aTip1, $aTip2) {
+        return ($aTip1['score'] < $aTip2['score']? 1 :
+            ($aTip1['score'] == $aTip2['score']? 0 : -1));
+    });
+
+    if (!$aResults) {
+        // No results.
+        tips_displayError('No results', 'There are no results for your search term. Try a different term, or <a href="https://www.LOVD.nl/contact?other" target="_blank">contact us</A>.');
+        return false;
+    }
+    $nMaxScore = max(20, $aResults[0]['score']);
+
+    // Fill in OG data to make the Facebook previews pretty.
+    tips_setOGData(array(
+        'title' => 'Search results',
+    ));
+
+    print('
+        <div class="card border-primary">
+            <div class="card-header">
+                Resultaten
+            </div>
+            <ul class="list-group list-group-flush">');
+    foreach ($aResults as $aTip) {
+        print('
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a href="' . tips_getLink($aTip['title']) . '">' . $aTip['title'] . '</a><span>' .
+            ((time() - strtotime($aTip['date'])) > (7 * 24 * 60 * 60)? '' : '
+                    <span class="badge badge-success">Recently updated!</span>') . '
+                    <span class="badge badge-primary badge-pill">' . round(($aTip['score']/$nMaxScore)*100) . '%</span></span>
+                </li>');
+    }
+    print('
+            </ul>
+        </div>');
+    return true;
+}
+
+
+
+
+
+function tips_showTags ($sURL)
+{
+    // Show tags or specific tag with links to posts.
+    global $_DATA;
+
+    // URL starts with 't/' or is just 't'.
+    $sThisTag = strtolower(substr($sURL, 2));
+    $bShowAll = (!$sThisTag);
+    $aAllTags = array();
+    $aThisTag = array();
+
+    foreach ($_DATA['tips'] as $aTip) {
+        foreach ($aTip['tags'] as $sTag) {
+            if (!isset($aAllTags[$sTag])) {
+                $aAllTags[$sTag] = 0;
+            }
+            $aAllTags[$sTag] ++;
+            if (!$bShowAll && $sThisTag == $sTag) {
+                $aThisTag[] = array($aTip['title'], $aTip['date']);
+            }
+        }
+    }
+    ksort($aAllTags);
+
+    if (!$aAllTags) {
+        // No tags exist anywhere.
+        tips_displayError('No tags', 'No tags have been provided yet.');
+        return false;
+    }
+
+    if (!$bShowAll) {
+        if (!$aThisTag) {
+            // Wrong or non-existent tag.
+            tips_displayError('Unknown tag', 'This tag is unknown. Go back to the home page or use the search form to find what you\'re looking for.');
+            return false;
+        } else {
+            $sTagLinks = '';
+            foreach ($aThisTag as list($sTitle, $sDate)) {
+                $sTagLinks .= '
+            <li class="list-group-item">' .
+                    ((time() - strtotime($sDate)) > (7 * 24 * 60 * 60)? '' : '
+                <span class="float-right badge badge-success">Recently updated!</span>') . '
+                <a href="' . tips_getLink($sTitle) . '">' . $sTitle . '</a>
+            </li>';
+            }
+            echo '
+        <div class="card border-primary">
+            <div class="card-header">
+                ' . ucfirst($sThisTag) . '
+            </div>
+            <ul class="list-group list-group-flush">' .
+                $sTagLinks . '
+            </ul>
+        </div>
+        <br><br>';
+        }
+    }
+
+    // Fill in OG data to make the Facebook previews pretty.
+    tips_setOGData(array(
+        'title' => ($bShowAll? 'All tags' : 'Tag: ' . $sThisTag),
+    ));
+
+    print('
+        <div class="card border-primary">
+            <div class="card-header">
+                Alle tags
+            </div>
+            <ul class="list-group list-group-flush">');
+    foreach ($aAllTags as $sTag => $nCount) {
+        print('
+                <li class="list-group-item d-flex justify-content-between align-items-center"><a href="t/' . $sTag . '">' . $sTag . '</a><span class="badge badge-primary badge-pill">' . $nCount . '</span></li>');
+    }
+    print('
+            </ul>
+        </div>');
+    return true;
+}
+?>
---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+// All of this code is copyrighted by Ivo Fokkema <Ivo@IFokkema.nl>,
+//  and developed and written for his private website epoxytips.nl.
+// This code has been licensed to the Leiden University Medical Center
+//  for use on retina.lovd.nl.
+
+setlocale(LC_CTYPE, 'en_US.utf8'); // needed for iconv.
+define('ROOT_PATH', './');
+require ROOT_PATH . 'inc-lib.php';
+
+// Find out whether or not we're using SSL.
+if ((!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') || (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || !empty($_SERVER['SSL_PROTOCOL'])) {
+    // We're using SSL!
+    define('PROTOCOL', 'https://');
+} else {
+    define('PROTOCOL', 'http://');
+}
+
+// Open JSON file.
+$sData = file_get_contents('data.json');
+if ($sData) {
+    $_DATA = json_decode($sData, true);
+}
+ob_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Retina.LOVD.nl</title>
+    <BASE href="<?php echo tips_getInstallURL(); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <meta property="og:title" content="">
+    <meta property="og:image" content="<?php echo tips_getInstallURL(); ?>gfx/og_image.png">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
+    <meta property="og:description" content="">
+    <meta property="og:locale" content="nl_NL">
+    <meta property="og:site_name" content="Retina.LOVD.nl">
+
+    <style type="text/css">
+        footer {background-color: rgba(0, 0, 0, 0.1);}
+        .card  {background-color: rgba(255, 255, 255, 0.5);}
+    </style>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background: #003660;">
+        <a class="navbar-brand font-weight-bold" href="">Retinal diseases genetic variants registry funded by Foundation Fighting Blindness</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="genes">Genes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="l/portal" target="_blank">Portal</a>
+                </li>
+            </ul>
+            <form class="form-inline" onsubmit="document.location.href='<?php echo tips_getInstallURL(); ?>'+'s/'+document.forms[0].search.value; return false;">
+                <div class="input-group">
+                    <input id="search" type="text" class="form-control" placeholder="Search term(s)">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-outline-light">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container my-4">
+
+<?php
+if (empty($_DATA)) {
+    tips_displayError('Maintenance', 'This site is being maintained at the moment.');
+} else {
+    $sURL = preg_replace(
+        '/^' . preg_quote(tips_getInstallURL(false), '/') . '/',
+        '',
+        strip_tags(rawurldecode($_SERVER['REQUEST_URI'])));
+    $sURL = strstr($sURL . '?', '?', true);
+    if (!$sURL) {
+        // No URL given. Go to the "why" page.
+        header('Location: ' . tips_getInstallURL() . 'home', 302);
+        exit;
+    }
+
+    // Special pages.
+    elseif (substr($sURL . '/', 0, 2) == 'c/') {
+        // Categories.
+        tips_showCategories($sURL);
+    }
+
+    elseif (substr($sURL . '/', 0, 2) == 'l/') {
+        // Links.
+        tips_showLinks($sURL);
+    }
+
+    elseif (substr($sURL . '/', 0, 2) == 's/') {
+        // Searching.
+        tips_showSearchResults($sURL);
+    }
+
+    elseif (substr($sURL . '/', 0, 2) == 't/') {
+        // Tags.
+        tips_showTags($sURL);
+
+    } else {
+        $aCodes = tips_getCodes();
+        if (isset($aCodes[$sURL])) {
+            tips_showPost(array('code' => $sURL));
+        }
+
+        // Otherwise, try to match with the titles (so I don't have to create codes).
+        else {
+            // Create a getTitles() and just sort through those,
+            //  looking for the best match.
+            $aTitles = tips_getTitles();
+            if (isset($aTitles[$sURL])) {
+                tips_showPost($aTitles[$sURL]);
+            }
+
+            // Nothing left to do, page not found.
+            else {
+                // Forward to a search.
+                header('Location: ' . tips_getInstallURL() . 's/' . rawurlencode($sURL), 302);
+                tips_displayError('Page not found', 'Page not found. Go back to the home page or use the search form to find what you\'re looking for.');
+            }
+        }
+    }
+}
+?>
+        <footer class="my-md-4 border-top px-lg-2" style="background: #003660;">
+            <small class="text-white">&copy; 2020-2021 Leiden University Medical Center (LUMC)</small>
+        </footer>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
+<?php
+ob_end_flush(); // Output everything.
+?>
